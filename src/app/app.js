35a9d774ceed2363603c358f936b2b696c080027
Shulaev Ivan <mail@ntdservice.ru>
2023-01-31 17:31:03 +0300

    initial

---
+++ src/app/app.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const popup = document.querySelector('.welcome-popup-container');
+    // popup.classList.remove('popup-hidden');
+    // popup.classList.add('popup-show');
+});
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './app/app';
+import './js/index';
+import './css/style.css'
